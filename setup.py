9b7cbf3087be82eef86852ab6c061b60d8f5897c
Rodri082 <117199195+Rodri082@users.noreply.github.com>
2025-03-28 16:56:24 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    "packages": [
+        "os", "ctypes", "subprocess", "sys", "requests", "platform", 
+        "winreg", "tempfile", "shutil", "threading", "datetime", 
+        "colorama", "urllib.parse", "tqdm", "tkinter", "tkinter.ttk", 
+        "tkinter.messagebox"
+    ],
+    "excludes": [],
+    "include_files": [],
+}
+
+setup(
+    name="ManagerOfficeScriptTool",
+    version="2.2",
+    description="Herramienta de Detección, Instalación y Desinstalación Automática de Office",
+    options={"build_exe": build_exe_options},
+    executables=[Executable(
+        "ManagerOfficeScriptTool.py", 
+        base="console", 
+        icon="icono.ico"
+    )],
+)
